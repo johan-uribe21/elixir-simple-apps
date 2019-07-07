6fd12cf36fc56bb8cd57f12d2c5b59e979adbada
johan-uribe21 <johanandresuribe@gmail.com>
2019-07-06 23:22:46 -0400

    finished card module

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,43 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"] #list
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    # For every value, match it with every suit
 
-  @doc """
-  Hello world.
+    # combined two nested comprehensions into one
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
 
-  ## Examples
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+# comment
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 
-      iex> Cards.hello()
-      :world
+  def deal(deck, hand_size) do
+    {hand, _pile} = Enum.split(deck, hand_size)
+    hand
+  end
 
-  """
-  def hello do
-    :world
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
